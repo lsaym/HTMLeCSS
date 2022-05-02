3d2614e98d55c0ea4cee57548ca6d7458a615e85
lucas saymon bezerra silva <saymin2266@gmail.com>
2022-05-02 20:49:17 -0300

    alt5

---
+++ TripMil/script.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+    window.document.getElementById("menu").classList.toggle("change")
+    window.document.getElementById("nav").classList.toggle("change")
+    window.document.getElementById("menu-bg").classList.toggle("change")
+}
