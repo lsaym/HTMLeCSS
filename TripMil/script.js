2ed7eb1a89f453796fe443712dc769da02fe5b7a
lucas saymon bezerra silva <saymin2266@gmail.com>
2022-05-02 22:09:45 -0300

    comentário sobre -bg

--- TripMil/script.js
+++ TripMil/script.js
@@ -1,5 +1,5 @@
 function onClickMenu(){
     document.getElementById("menu").classList.toggle("change")
     document.getElementById("nav").classList.toggle("change")
-    document.getElementById("menu-bg").classList.toggle("change-bg")
+    document.getElementById("menu-bg").classList.toggle("change-bg")// Atenção ao -bg no change pra alteração quando a função for solicitada com o click
 }
