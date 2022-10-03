2e02f586543fa59087a56e1bda699c3ae6afbdf4
lucas saymon bezerra silva <saymin2266@gmail.com>
2022-10-03 14:16:20 -0300

    map

--- JAVASCRIPT/map.js
+++ JAVASCRIPT/map.js
@@ -1,4 +1,4 @@
 const cursos=['HTML','CSS','JAVASCRIPT','PHP', 'REACT']
 cursos.map((el,i)=>{
-    console.log("curso: " + el +" posição do curso:"+ i)
+console.log("curso: " + el +" posição do curso:"+ i)
 })
