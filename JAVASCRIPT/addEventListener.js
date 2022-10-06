afabf636096db360cca03d3c617ecbebca265274
lucas saymon bezerra silva <saymin2266@gmail.com>
2022-10-05 21:54:02 -0300

    mais itens de formulário

---
+++ JAVASCRIPT/addEventListener.js
@@ -0,0 +1,14 @@
+
+const cursos = [...document.querySelectorAll(".curso")]
+
+
+cursos.map((el) => {
+    el.addEventListener("click", (evt) => {
+        const el = evt.target
+        el.classList.add("destaque")
+        console.log(el.id + " foi clicado")
+        el.innerHTML = "clicou"
+
+    })
+})
+
